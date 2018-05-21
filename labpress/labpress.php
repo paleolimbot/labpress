c9f8911c0935fc06431f4b30ac7ce835ec3ff937
Dewey Dunnington <dewey@fishandwhistle.net>
2018-05-20 23:14:06 -0300

    Initial commit

---
+++ labpress/labpress.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: labpress
+Plugin URI: https://github.com/paleolimbot/labpress
+Description: Laboratory sample management plugin for wordpress sites.
+Version: 1.0.0
+Author: Dewey Dunnington
+Author URI: http://www.fishandwhistle.net/
+License: GPLv2
+*/
+
